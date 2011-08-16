541f101943c6419aa0e0e3168721aa27bc011264
Alireza Khodadad <alireza.khodadad@ryerson.ca>
2011-08-16 12:21:33 -0400

    wtf happened

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
   
     @title = "Sign up"
   end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,5 +41,21 @@ describe UsersController do
     end
 
   end
-
+  describe "POST 'create'" do
+  
+    describe "success" do
+      before(:each) do
+	@attr = {:name => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" }
+      end
+    it "should create a user" do
+      lambda do
+        post :create, :user => @attr
+      end.should change(User, :count).by(1)
+    end
+    it "should redirect to the user show page" do
+      post :create, :user => @attr
+      response.should redirect_to(user_path(assigns(:user)))
+    end
+   end
+  end
 end
